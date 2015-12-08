e782bf82e0b0c81d92bde7cf7808c214d80e6db3
Simon Treutlein <git@inf-git.fh-rosenheim.de:prg3-ws15/Bauhaus_Lagerverwaltung.git>
2015-12-08 19:00:12 +0100

    TestProjekt

---
+++ Projekt/Projekt/Objektverwaltung/Getraenke/ENUMgetraenkeArt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Objektverwaltung.Getraenke
+{
+    public class ENUMgetraenkeArt
+    {
+
+        public enum getraenkeArt
+        {
+            Bier,Wein,Schnaps,Saft,Softdrink,Wasser,Kaffee
+        }
+    }
+}
---
+++ Projekt/Projekt/Objektverwaltung/Snacks/ENUMsnackArt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Objektverwaltung.Snacks
+{
+    public class ENUMsnackArt
+    {
+        public enum snackArt
+        {
+            Gummibaer, Erdnuesse, Salzstangen, ErdnussFlips, Chips
+        }
+
+    }
+}
