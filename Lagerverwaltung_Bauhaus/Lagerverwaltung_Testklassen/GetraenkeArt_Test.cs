a2775340cd22407d69091ca0cb1a1c43248f65e2
Simon Treutlein <git@inf-git.fh-rosenheim.de:prg3-ws15/Bauhaus_Lagerverwaltung.git>
2016-01-18 22:03:42 +0100

    commit klassen von robin

---
+++ Lagerverwaltung_Bauhaus/Lagerverwaltung_Testklassen/GetraenkeArt_Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lagerverwaltung_Bauhaus.Lagerhaltung.Produkte.Getränke.alkoholisch;
+using Lagerverwaltung_Bauhaus.Lagerhaltung.Produkte.Getränke;
+
+namespace Lagerverwaltung_Testklassen
+{
+    public class GetraenkeArt_Test
+    {
+        public void bierTest()
+        {
+            Bier bier = new Getraenk(0.5, true, 1,  "Augustiner", "Bier",  "Lager") as Bier;
+
+
+            string getraenkart = bier.GetraenkeArt;
+
+        }
+        
+    }
+}
---
+++ Lagerverwaltung_Bauhaus/Lagerverwaltung_Testklassen/Parametertest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lagerverwaltung_Bauhaus.Lagerhaltung.Produkte.Getränke;
+namespace Lagerverwaltung_Testklassen
+{
+    [TestClass]
+    public class Parametertest
+    {
+      
+        
+            
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Getraenk g = new Getraenk();
+
+            g.erstelleGetraenk(g.Fuellmenge = 0.5, g.Alkohol = true, g.Anzahl = 1, g.GetraenkeName = "Augustiner", g.GetraenkeArt = "Bier",g.Lagerort = "Lager");
+
+
+            //Erwartete Ausgabe: Lagerort
+            string abc = g.Lagerort;
+            Console.WriteLine(abc);
+            Console.ReadKey();
+        }
+    }
+}
